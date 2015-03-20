2dac9880373c5e057c02f70e41848b70e1318036
AmjadIqbal347 <amjad6as@gmail.com>
2015-03-20 04:43:20 -0700

    Udacity_FSD_Project_1
    Movie Trailer Website

---
+++ movies/media.py
@@ -0,0 +1,10 @@
+import webbrowser
+class Movie():
+    def __init__(self,movie_title,movie_storyline,movie_poster,movie_trailer):
+        self.title=movie_title
+        self.storyline=movie_storyline
+        self.poster_image_url=movie_poster
+        self.trailer_youtube_url=movie_trailer
+
+    def showtrailer(self):
+        webbrowser.open(self.trailer)
